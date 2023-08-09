40a058d28300a86486f741eed277e060f6acd271
Fabian <wenzelfabi@web.de>
2023-08-09 17:58:15 +0200

    Benutzeroberfläche_test

---
+++ src/monopol/test/Benutzeroberflaeche_Test.java
@@ -0,0 +1,59 @@
+package monopol.test;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.net.URL;
+
+public class Benutzeroberflaeche_Test {
+
+    public static void main(String[] args) {
+        JFrame frame = frame();
+        bild(frame);
+        Text(frame);
+        Knopf(frame);
+        frame.setVisible(true);
+    }
+
+    private static JFrame frame(){
+        JFrame frame = new JFrame("Monopoly");
+        frame.setSize(800, 600);
+        return frame;
+    }
+
+    private static void bild(JFrame frame) {
+
+        String pfad = "Bilder/Monopoly.png";
+
+        ImageIcon imageIcon = new ImageIcon(pfad);
+        Image normalImage = imageIcon.getImage();
+        int breite = 250;
+        int Hoehe = 60;
+        Image skaliertesImage = normalImage.getScaledInstance(breite, Hoehe, Image.SCALE_SMOOTH);
+        JLabel imageLabel2 = new JLabel(new ImageIcon(skaliertesImage));
+        imageLabel2.setBounds(300, 10, breite, Hoehe);
+        frame.add(imageLabel2);
+        frame.setLayout(null);
+    }
+
+    private static void Text(JFrame frame){
+        String text = "Mein test Text!";
+        JLabel label = new JLabel(text);
+        label.setFont(new Font("Arial", Font.PLAIN, 16));
+        label.setBounds(100, 200, 300, 50);
+        frame.add(label);
+    }
+
+    private static void Knopf(JFrame frame){
+        JButton knopf = new JButton("Klick mich!");
+        knopf.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "YAY!");
+            }
+        });
+        knopf.setBounds(300, 400,200,80);
+        frame.add(knopf);
+    }
+}
+
+
