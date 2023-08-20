dff685147288b126bb531d35530606ae968017cd
BuildTools <unconfigured@null.spigotmc.org>
2023-08-20 14:38:40 +0200

    Logger level: config

--- src/monopol/log/ServerLogger.java
+++ src/monopol/log/ServerLogger.java
@@ -1,11 +1,13 @@
 package monopol.log;
 
+import java.util.logging.Level;
+
 public class ServerLogger extends CustomLogger {
     public static final ServerLogger INSTANCE = new ServerLogger();
 
     private ServerLogger() {
         super("logs/server_log.txt");
-        //getLogger().setLevel(Level.CONFIG);
+        getLogger().setLevel(Level.CONFIG);
     }
 
     @Override
