67c2245b7f24ff7815eaac71e08c4b1bf5d223e4
BuildTools <unconfigured@null.spigotmc.org>
2024-05-17 19:25:56 +0200

    Monopoly Jar

--- src/main/java/monopol/common/packets/Packet.java
+++ src/main/java/monopol/common/packets/Packet.java
@@ -1,24 +1,7 @@
 package monopol.common.packets;
 
-import monopol.client.Client;
 import monopol.common.data.DataWriter;
-import monopol.common.message.Message;
 
-import java.util.function.Consumer;
-import java.util.function.Function;
-
-/**
- * A packet is a container of a message. All attributes need to be
- * (de)serialized to be transported over a network. To do so, you
- * need to create the following deserialization method: <b>
- * {@code public static T deserialize(Object[]args)}</b>. This method is
- * executed via java reflect (see {@link PacketManager#handle(Message, Side)}). Packets
- * are sent via {@link PacketManager#sendS2C(S2CPacket, Function, Consumer)}
- * and {@link PacketManager#sendC2S(C2SPacket, Client, Consumer)}. Once
- * received, the {@link Packet#handle(Side)} method is executed.
- *
- * @param <T> the packets class
- */
 public interface Packet<T extends Packet<T>> {
     void serialize(DataWriter writer);
     void handle(Side side);
